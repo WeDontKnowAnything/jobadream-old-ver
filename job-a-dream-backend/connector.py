4c8af74068334962a3aba91675c0d6755b412dcb
artemIntellectus <smoh17@naver.com>
2024-08-28 20:36:23 +0900

    fix: databaseurl 경로 수정

--- job-a-dream-backend/connector.py
+++ job-a-dream-backend/connector.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 환경 변수에서 DATABASE_URL 가져오기
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
