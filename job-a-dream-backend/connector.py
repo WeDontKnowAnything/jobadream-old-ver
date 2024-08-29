1ef447f1d7bd2a0ab843f51908a12260b0ac5f22
artemIntellectus <smoh17@naver.com>
2024-08-29 16:38:03 +0900

    test: no-cache 명령어 추가

--- job-a-dream-backend/connector.py
+++ job-a-dream-backend/connector.py
@@ -1,7 +1,7 @@
 import os
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
