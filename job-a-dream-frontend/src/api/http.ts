23f929a926ac3bdbe0b9163eb3306b5c3a819dbe
Hon2yB22 <qjrm1430@naver.com>
2024-09-05 15:40:55 +0900

    hotfix: change baseurl

--- job-a-dream-frontend/src/api/http.ts
+++ job-a-dream-frontend/src/api/http.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
-// const BaseURL: string = 'api.jobadream.com'
-// const Port = 443
-const BaseURL: string = location.host.split(':')[0]
-const Port = 8000
+const BaseURL: string = 'api.jobadream.com'
+const Port = 443
 
-// const http = axios.create({ baseURL: `https://${BaseURL}:${Port}` })
-const http = axios.create({ baseURL: `http://${BaseURL}:${Port}` })
+// const BaseURL: string = location.host.split(':')[0]
+// const Port = 8000
+
+const http = axios.create({ baseURL: `https://${BaseURL}:${Port}` })
+
+// const http = axios.create({ baseURL: `http://${BaseURL}:${Port}` })
 export { http }
